6a672e55077c2760dd89e82ec6a940c533c25bbc
Richard Stelling <richard@empiricalmagic.com>
2014-02-10 10:47:41 +0000

    - Updated build settings to match those in the project where the issue was discovered.

--- optimisation-bug/optimisation-bug/BUGViewController.m
+++ optimisation-bug/optimisation-bug/BUGViewController.m
@@ -14,11 +14,27 @@
 @end
 
 @implementation BUGViewController
+{
+}
 
 - (void)viewDidLoad
 {
+#ifdef DEBUG
+# warning Compiling with no optmisations, the code should work as expected
+    NSLog(@"*** Compiling with no optmisations, the code should work as expected ***");
+#endif
+
+#ifdef RELEASE
+# warning Compiling with -Os optmisations, the code should *NOT* work as expected
+    NSLog(@">>> Compiling with -Os optmisations, the code should *NOT* work as expected <<<");
+#endif
+    
     [super viewDidLoad];
-	
+    
+    CGRect rect;
+    CGSize size;
+    CGPoint point;
+    
     //Use the category to encode the numbers
     NSNumber *encodedRect = [NSNumber numberWithRect:CGRectMake(0, 0, 768, 1024)];
     NSNumber *encodedSize = [NSNumber numberWithSize:CGSizeMake(640, 1136)];
@@ -29,13 +45,19 @@
     NSLog(@"encodePoint: %@", encodePoint);
     
     //Decodeing should fail if optmisation level is set to -Os or -O4
-    CGRect rect = [encodedRect rectValue];
-    CGSize size = [encodedSize sizeValue];
-    CGPoint point = [encodePoint pointValue];
+    rect = [encodedRect rectValue];
+    size = [encodedSize sizeValue];
+    point = [encodePoint pointValue];
     
-    NSLog(@"rect: %@", NSStringFromCGRect(rect));
-    NSLog(@"size: %@", NSStringFromCGSize(size));
-    NSLog(@"point: %@", NSStringFromCGPoint(point));
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(point.x, point.y, size.width, size.height)];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    
+//    NSLog(@"rect: %@", NSStringFromCGRect(rect));
+//    NSLog(@"size: %@", NSStringFromCGSize(size));
+//    NSLog(@"point: %f, %f", point.x, point.y);
+//    
+    NSLog(@"DONE");
 }
 
 - (void)didReceiveMemoryWarning
